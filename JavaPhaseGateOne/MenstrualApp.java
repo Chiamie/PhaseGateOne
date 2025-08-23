3fee4e8f85e91f79179d41c44760a8c470651c18
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-23 16:37:00 +0100

    in prgress

--- JavaPhaseGateOne/MenstrualApp.java
+++ JavaPhaseGateOne/MenstrualApp.java
@@ -1,5 +1,11 @@
 public class MenstrualApp {
+public static void main(String[] args){
 
+int numberOfMonth = 1;
+int numberOfDays = getDaysIn(numberOfMonth);
+String startDay = "Mon";
+System.out.println(displayCalendar(numberOfDays, startDay));
+}
 
 
 
@@ -11,36 +17,53 @@ public static int getDaysIn(int numberOfMonth){
 	else if (numberOfMonth == 1 || numberOfMonth == 3 || numberOfMonth == 5 || numberOfMonth == 7 || numberOfMonth == 8 || numberOfMonth == 10 || numberOfMonth == 11)
 		numberOfDaysPerMonth = 31;
 	else
-		numberOfDaysPerMonth = 28 || numberOfDaysPerMonth = 29;
-	return numberOfDaysPerMonth
+		numberOfDaysPerMonth = 28;
+	return numberOfDaysPerMonth;
 }
 
 
 
 
 public static String getDaysOfWeek(){
-	String[] daysOfTheWeek = {'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'};
-	for(int index = 0; index < daysOfTheWeek.length; index++){
-		return String.join(" ", daysOfTheWeek);
+	String[] daysOfTheWeek = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};	
+	return String.join("\t", daysOfTheWeek);
+}
+
+public static int getStartDayIndex(String startDay){
+	String[] daysOfTheWeek = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};	
+	int startIndex = 0;
+	for(int number = 1; number < daysOfTheWeek.length ; number++){
+		if(startDay.equals(daysOfTheWeek[number])) 
+		startIndex = number;
 	}
-}
-
-public static int displayCalendar(int numberOfDays,  ){
-	getDaysOfWeek();
-	
-
-
+	return startIndex;
 }
 
 
+public static String displayCalendar(int numberOfDays, String startDay){
+	System.out.println(getDaysOfWeek());
+	int startIndex = getStartDayIndex(startDay);
+	String calendar = "";
+	for (int index = 0; index < startIndex; index++) {
+		calendar += "\t";     	
+       }
+
+	for(int number = 1; number <= numberOfDays; number++){
+		calendar += String.format("%d \t", number);
+		if((number + startIndex) % 7 == 0) {
+			calendar += "\n";
+		}
+			
+	}
+
+	return calendar;
+
+}
 
 
 
 
-
-
-
-
+    
 
 
 
