f1abeec51420570d12d2bb1d54c52d2a09d26874
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-24 15:14:04 +0100

    added the doubleDigitsAtEvenIndexIn Function

--- JavaPhaseGateOne/CreditCardValidator.java
+++ JavaPhaseGateOne/CreditCardValidator.java
@@ -7,17 +7,31 @@ public static void main(String[] args){
 
 }
 
-public static int getArrayFormOf(int cardNumber){
-	String cardNumberAsString = "" + cardNumber;
-	String[] cardNumberAsStringArray = cardNumberAsString.split("");
+public static int[] getArrayFormOf(String cardNumber){
+	String[] cardNumberAsStringArray = cardNumber.split("");
 	int[] cardNumberAsArray = new int[cardNumberAsStringArray.length];
 	for(int index = 0; index < cardNumberAsArray.length; index++){
-		cardNumberAsArray[index] = Integer.parseInt(cardNumberAsArray[index]);
+		cardNumberAsArray[index] = Integer.parseInt(cardNumberAsStringArray[index]);
 	}
 	return cardNumberAsArray;
 		
 }
 
+public static int doubleDigitsAtEvenIndexIn(int[] cardNumberAsArray){
+	int sum = 0;
+	for(int index = cardNumberAsArray.length - 1; index >= 0; index--){
+		int product = 1;
+		if(index % 2 == 0){
+			if (cardNumberAsArray[index] % 10 != 0){
+				product = cardNumberAsArray[index] * 2;
+				sum += (product % 10) + (product / 10);
+			}else{
+				sum += cardNumberAsArray[index] * 2;
+			}
+		}
+	}
+	return sum;
+}
 
 
 
--- JavaPhaseGateOne/CreditCardValidatorTest.java
+++ JavaPhaseGateOne/CreditCardValidatorTest.java
@@ -1,19 +1,31 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 
 
 public class CreditCardValidatorTest{
 @Test	
-public void testgetDaysInFunction(){
-	int cardNumber = 5399831619690403;
+public void testGetDaysInFunction(){
+	String cardNumber = "5399831619690403";
 		
 	CreditCardValidator creditCardValidator = new CreditCardValidator();
-	int result = creditCardValidator.getArrayFormOf(cardNumber);
+	int[] result = creditCardValidator.getArrayFormOf(cardNumber);
+	int[] expected = new int[]{5, 3, 9, 9, 8, 3, 1, 6, 1, 9, 6, 9, 0, 4, 0, 3};
 	
-	assertEquals(result, {5, 3, 9, 9, 8, 3, 1, 6, 1, 9, 6, 9, 0, 4, 0, 3});
+	assertArrayEquals(result, expected);
+							
 }
 
-
+@Test	
+public void testdoubleDigitsAtEvenIndexInFunction(){
+	int[] cardNumberAsArray = {5, 3, 9, 9, 8, 3, 1, 6, 1, 9, 6, 9, 0, 4, 0, 3};
+		
+	CreditCardValidator creditCardValidator = new CreditCardValidator();
+	int result = creditCardValidator.doubleDigitsAtEvenIndexIn(cardNumberAsArray);
+	
+	
+	assertEquals(result, 24);
+							
+}
 }
 
