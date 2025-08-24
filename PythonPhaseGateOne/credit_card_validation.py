d45a73b206e593631a7a8537d4478c29d7a3e343
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-24 21:47:25 +0100

    added check_card_number_length function

--- PythonPhaseGateOne/credit_card_validation.py
+++ PythonPhaseGateOne/credit_card_validation.py
@@ -19,6 +19,22 @@ def check_type_of_card(card_number):
 	else:
 		return "Invalid card"
 
+def check_card_number_length(card_number):
+	if type(card_number) != str:
+		raise TypeError("card_number must be a string")
+	if card_number == "":
+		raise ValueError("card_number cannot be empty")
+	for item in card_number:
+		if not item.isdigit():
+			raise ValueError("card_number must be all numeric strings")
+
+	if len(card_number) >= 13 and len(card_number) <= 16:
+		card_length = str(len(card_number))
+		return card_length
+	else:
+		return "Invalid Card Number"
+	
+
 
 
 
