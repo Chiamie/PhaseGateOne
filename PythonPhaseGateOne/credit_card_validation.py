9d268a66f879597e4546abc3567c076fe42bbe61
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-24 21:04:00 +0100

    added check_type_of_card function

---
+++ PythonPhaseGateOne/credit_card_validation.py
@@ -0,0 +1,28 @@
+
+def check_type_of_card(card_number):
+	if type(card_number) != str:
+		raise TypeError("card_number must be a string")
+	if card_number == "":
+		raise ValueError("card_number cannot be empty")
+	for item in card_number:
+		if not item.isdigit():
+			raise ValueError("card_number must be all numeric strings")
+
+	if card_number[0] == '4':
+		return "Visa Card"
+	elif card_number[0] == '5':
+		return "MasterCard"
+	elif card_number[0] == '6':
+		return "Discover cards"
+	elif card_number[0] == '3' and card_number[1] == '7':
+		return "American Express Card"
+	else:
+		return "Invalid card"
+
+
+
+
+
+
+
+
---
+++ PythonPhaseGateOne/test_credit_card_validation.py
@@ -0,0 +1,37 @@
+import unittest
+import inspect
+from credit_card_validation import check_type_of_card
+
+
+
+
+class TestCreditCardValidation(unittest.TestCase):
+	def test_that_the_check_type_of_card_function_accepts_1_argument(self):
+		function_signature = inspect.signature(check_type_of_card)
+		dic_parameter = function_signature.parameters
+		self.assertEqual(len(dic_parameter), 1)
+	def test_that_the_check_type_of_card_function_raises_validation_when_argument_is_a_float(self):
+		self.assertRaises(TypeError, check_type_of_card, 32.8)
+	def test_that_the_check_type_of_card_function_raises_validation_when_argument_is_an_integer(self):
+		self.assertRaises(TypeError, check_type_of_card, 32)
+	def test_that_the_check_type_of_card_function_raises_validation_when_argument_is_a_non_numeric_string(self):
+		self.assertRaises(TypeError, check_type_of_card, "mummy")
+	def test_that_the_check_type_of_card_function_raises_validation_when_argument_is_a_non_numeric_string(self):
+		self.assertRaises(ValueError, check_type_of_card, "")
+	def test_that_the_check_type_of_card_function_returns_correct_result(self):
+		result = check_type_of_card("5399831619690404")
+		self.assertEqual(result, 'MasterCard')
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
