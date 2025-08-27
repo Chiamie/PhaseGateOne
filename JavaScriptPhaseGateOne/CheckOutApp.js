46c0063de924f61530009dc360845c94f9c27858
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-27 15:53:01 +0100

    output not yet aligned

--- JavaScriptPhaseGateOne/CheckOutApp.js
+++ JavaScriptPhaseGateOne/CheckOutApp.js
@@ -31,8 +31,13 @@ function getVATOf(subTotal){
 
 
 function getBillTotal(subTotal, discountAmount, VATAmount){
-	return subTotal + discountAmount + VATAmount;
+	return (subTotal - discountAmount) + VATAmount;
 }
+
+function getBalance(amountReceived, billTotal){
+	return billTotal - amountReceived;
+}
+
 
 
 const prompt = require('prompt-sync')();
@@ -76,19 +81,27 @@ let subTotal = getSubTotal(userCart);
 let discountAmount = getDiscountOf(subTotal, discount);
 let VATAmount = getVATOf(subTotal);
 let billTotal = getBillTotal(subTotal, discountAmount, VATAmount);
-let nameOfStore = "SEMICOLON STORES";
+
+let nameOfStore = `SEMICOLON STORES
+				MAIN BRANCH
+			`;
 let storeAddress = "312, HERBERT MACAULAY WAY, SABO YABA, LAGOS"
 let contact = "03293828343";
 let date = new Date();
 
 
-console.log(");
+console.log(nameOfStore);
+console.log("LOCATION: " + storeAddress);
+console.log("TEL: " + contact);
+console.log("Date: " + date);
+console.log("Cashier: " + cashierName);
+
 console.log("Customer Name: " + userName);
 
 
 
 
-console.log("--".repeat(5));
+console.log("--".repeat(10));
 let columns = Object.keys(userCart[0]);
 console.log("\t".repeat(5) + columns.join("\t"));
 userCart.forEach(row => {console.log("\t".repeat(4) + Object.values(row).join("\t"));
@@ -99,12 +112,28 @@ console.log("\t".repeat(4) + "VAT @ 17.50%" + VATAmount);
 console.log("\t".repeat(4) + "Bill Total" + billTotal);
 
 console.log("THIS IS NOT A RECEIPT KINDLY PAY" + billTotal);
-console.log("THIS IS NOT A RECEIPT KINDLY PAY" + billTotal);
 
 
 
 
 
+let amountReceived = prompt("How much did the customer give to you? ");
+let balance = getBalance(amountReceived, billTotal)
+
+console.log("--".repeat(10));
+columns = Object.keys(userCart[0]);
+console.log("\t".repeat(5) + columns.join("\t"));
+userCart.forEach(row => {console.log("\t".repeat(4) + Object.values(row).join("\t"));
+});
+console.log("\t".repeat(4) + "Sub Total: " + subTotal);
+console.log("\t".repeat(4) + "Discount: " + discountAmount);
+console.log("\t".repeat(4) + "VAT @ 17.50%: " + VATAmount);
+console.log("\t".repeat(4) + "Bill Total: " + billTotal);
+console.log("\t".repeat(4) + "Amount Paid: " + amountReceived);
+console.log("\t".repeat(4) + "Balance: " + balance);
+
+
+console.log("THANK YOU FOR YOUR PATRONAGE");
 
 
 
