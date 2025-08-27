a0ed54f959b5e2aa77f9a4b154b32dd26f89b807
Chiamie <okechukwurchiamaka@gmail.com>
2025-08-27 15:18:14 +0100

    output not yet aligned

---
+++ JavaScriptPhaseGateOne/CheckOutApp.js
@@ -0,0 +1,154 @@
+function getProductDetails(itemName, itemQuantity, itemPrice, itemTotal){
+	let productDetails = {};
+	productDetails.ITEM = itemName;
+	productDetails.QTY = itemQuantity;
+	productDetails.PRICE = itemPrice;
+	productDetails.TOTAL = itemTotal;
+	return productDetails;
+}
+
+function getSubTotal(userCart){
+	let sum = 0;
+	for(let index = 0; index < userCart.length; index++){
+		for(let detail in userCart[index]){
+			if (detail == "TOTAL"){
+				sum += userCart[index][detail];
+			}
+		}
+	}
+	return sum;
+}
+	
+function getDiscountOf(subTotal, discount){
+	let discountAmount = subTotal * (discount / 100);
+	return discountAmount;
+}
+
+function getVATOf(subTotal){
+	let VATAmount = subTotal * (17.50 / 100);
+		return VATAmount;
+}
+
+
+function getBillTotal(subTotal, discountAmount, VATAmount){
+	return subTotal + discountAmount + VATAmount;
+}
+
+
+const prompt = require('prompt-sync')();
+//let productDetails = {};
+let userCart = [];
+
+
+
+
+let userName;
+let itemName;
+let itemQuantity;
+let itemPrice;
+let itemTotal;
+let discount;
+let cashierName;
+
+
+userName = prompt("What is the customer's Name? ");
+
+itemName = prompt("What did the customer buy? ");
+itemQuantity = prompt("How many pieces? ");
+itemPrice = prompt("How much per unit? ");
+itemTotal = itemPrice * itemQuantity;
+
+
+userCart.push(getProductDetails(itemName, itemQuantity, itemPrice, itemTotal));
+let addMoreItems = prompt("Add more Items? ");
+while(addMoreItems.toLowerCase() == "yes"){
+	itemName = prompt("What did the customer buy? ");
+	itemQuantity = prompt("How many pieces? ");
+	itemPrice = prompt("How much per unit? ");
+	itemTotal = itemPrice * itemQuantity;
+	userCart.push(getProductDetails(itemName, itemQuantity, itemPrice, itemTotal));
+	addMoreItems = prompt("Add more Items? ");
+}
+
+cashierName = prompt("What is your name? ");
+discount = prompt("How much discount will he get? ");
+let subTotal = getSubTotal(userCart);
+let discountAmount = getDiscountOf(subTotal, discount);
+let VATAmount = getVATOf(subTotal);
+let billTotal = getBillTotal(subTotal, discountAmount, VATAmount);
+let nameOfStore = "SEMICOLON STORES";
+let storeAddress = "312, HERBERT MACAULAY WAY, SABO YABA, LAGOS"
+let contact = "03293828343";
+let date = new Date();
+
+
+console.log(");
+console.log("Customer Name: " + userName);
+
+
+
+
+console.log("--".repeat(5));
+let columns = Object.keys(userCart[0]);
+console.log("\t".repeat(5) + columns.join("\t"));
+userCart.forEach(row => {console.log("\t".repeat(4) + Object.values(row).join("\t"));
+});
+console.log("\t".repeat(4) + "Sub Total" + subTotal);
+console.log("\t".repeat(4) + "Discount" + discountAmount);
+console.log("\t".repeat(4) + "VAT @ 17.50%" + VATAmount);
+console.log("\t".repeat(4) + "Bill Total" + billTotal);
+
+console.log("THIS IS NOT A RECEIPT KINDLY PAY" + billTotal);
+console.log("THIS IS NOT A RECEIPT KINDLY PAY" + billTotal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
